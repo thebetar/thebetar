d2b6586e16d059d65cc2a968fce378f653799ebc
Lars Vonk <larserikvonk@ziggo.nl>
2023-11-17 09:50:55 +0100

    chore: simplified setup by removing neovim since mostly vim is used

--- install.sh
+++ install.sh
@@ -4,17 +4,6 @@ cp -r ./vim/.vim/* ~/.vim                 # Add VIM plugins
 cp ./vim/.vimrc ~/.vimrc                # Overwrite Vim configuration
 
 # OH MY ZSH CONFIG
-
-if [ ! -d "$HOME/.local/share/fonts" ]; then
-  ## Install fonts needed for zsh config
-  echo "[Status] Installing fonts"
-  git clone https://github.com/powerline/fonts &&
-    \ ./fonts/install.sh &&
-    \ rm -rf fonts
-else
-  echo "[Status] Fonts already installed"
-fi
-
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   ## Install oh-my-zsh
   echo "[Status] Installing oh-my-zsh"
@@ -26,22 +15,3 @@ fi
 ## Copy over standard config
 echo "[Status] Copying over zsh config"
 cp ./.zshrc ~/.zshrc
-
-# # Install neovim since it does not come preinstalled
-# if [ ! -f "~/.config/nvim" ]; then
-#   mkdir tmp
-#   cd tmp
-
-#   curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-#   chmod u+x nvim.appimage
-
-#   ./nvim.appimage --appimage-extract
-#   ./squashfs-root/AppRun --version
-#   sudo mv squashfs-root /
-#   sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
-# fi
-
-# # Install neovim settings
-# if [ -f "~/.config/nvim" ]; then
-#   cp -r ./nvim ~/.config/nvim
-# fi
