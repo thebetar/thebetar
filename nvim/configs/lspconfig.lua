410067c06f43cac905e037c118be61f74b9f2620
thebetar <larserikvonk@ziggo.nl>
2024-03-27 18:34:10 +0100

    chore: updated vscode and added custom nvchad config

---
+++ nvim/configs/lspconfig.lua
@@ -0,0 +1,29 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- if you just want default config for the servers then put them in a table
+local servers = { "html", "cssls", "clangd", "asm_lsp" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+lspconfig.eslint.setup{
+  filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact'},
+
+  on_attach = function(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end
+}
+
+lspconfig.volar.setup{
+  filetypes = {'vue', 'json'}
+}
---
+++ nvim/init.lua
@@ -0,0 +1,6 @@
+local vimopt = vim.opt
+
+vimopt.relativenumber = true
+vimopt.cursorcolumn = true
+vimopt.cursorline = true
+vimopt.scrolloff = 12
